e6625d20f204d01b07ba906832f3075e87a7b6df
Huijbregts,Servi S.P.G.I <servi.huijbregts@student.fontys.nl>
2022-12-16 20:01:16 +0100

    Log in to see pictures

--- src/App.js
+++ src/App.js
@@ -205,19 +205,20 @@ function App() {
       </div>
 
       <div className="timeline">
-        {user && <ImageUpload user={user} />}
+        {user && <ImageUpload user={user} /> }
+        {user && <div> {posts.map(({ id, post }) => (
+              <Post
+                  key={id}
+                  postId={id}
+                  user={user} // To pass current user to add current user when adding comment
+                  username={post.username}
+                  caption={post.caption}
+                  imageUrl={post.imageUrl}
+                  mission={post.mission}
+              />
+        ))} </div>}
 
-        {posts.map(({ id, post }) => (
-          <Post
-            key={id}
-            postId={id}
-            user={user} // To pass current user to add current user when adding comment
-            username={post.username}
-            caption={post.caption}
-            imageUrl={post.imageUrl}
-            mission={post.mission}
-          />
-        ))}
+
       </div>
     </div>
   );
